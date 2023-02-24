8362f744c610f4ea89cbe38a128c14c1d5438553
vrothx00 <vrothx00@gmail.com>
2023-02-24 10:54:02 +0100

    Added exer data types module

---
+++ data-types/strings_exercise.js
@@ -0,0 +1,7 @@
+// Write 3 examples of expressions.
+// One using + (concatenation), another using [] (indexing), and another using .length.
+// Print out the result of your expressions using console.log
+
+console.log("Tom " + "and " + "Jerry");
+console.log("Thomas"[1]);
+console.log("Jerry".length);
