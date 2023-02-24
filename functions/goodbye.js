8e8bd80d9fe6a28e7f3ed21ac6d82ff674f0d03c
vrothx00 <vrothx00@gmail.com>
2023-02-24 11:48:51 +0100

    Added functions assign

---
+++ functions/goodbye.js
@@ -0,0 +1,11 @@
+// Write a function goodbye(name) that takes in a string name and returns
+// a string saying bye to that name.
+
+function goodbye(name) {
+	// your code here...
+	return `Bye ${name}.`;
+  }
+
+  console.log(goodbye("Daniel")); // => "Bye Daniel."
+  console.log(goodbye("Mark")); // => "Bye Mark."
+  console.log(goodbye("Beyonce")); // => "Bye Beyonce."
