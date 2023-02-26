2f2610c2a2f204e739f0be84aab2e346667eec14
vrothx00 <vrothx00@gmail.com>
2023-02-26 19:18:25 +0100

    Added loop trans assign

---
+++ string_loops/fr_loop_trans.js
@@ -0,0 +1,24 @@
+function aCounter(word) {
+	// let index = 0;
+	let count = 0;
+	// while (index < word.length) {
+	//   let char = word[index];
+	//   if (char === "a" || char === "A") {
+	// 	count += 1;
+	//   }
+	//   index++;
+	// }
+
+	for(let i = 0; i < word.length; i++) {
+		let char = word[i];
+		if (char === "a" || char === "A") {
+			count += 1;
+		}
+	}
+
+	return count;
+  };
+
+  console.log(aCounter("apple")); // => 1
+  console.log(aCounter("appleapple")); // => 2
+  console.log(aCounter("aAapple")); // => 3
---
+++ string_loops/wh_loop_trans.js
@@ -0,0 +1,25 @@
+function eCounter(word) {
+  let count = 0;
+  let i = 0;
+
+//   for (let index = 0; index < word.length; index++) {
+//     let char = word[index];
+//     if (char === "e" || char === "E") {
+//       count += 1;
+//     }
+//   }
+
+  while(i < word.length) {
+	let char = word[i];
+	if (char === "e" || char === "E") {
+		count += 1;
+	}
+	i++;
+  }
+
+  return count;
+};
+
+console.log(eCounter("apple")); // => 1
+console.log(eCounter("appleapple")); // => 2
+console.log(eCounter("Appleee")); // => 3
